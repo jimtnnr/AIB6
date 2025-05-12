d01f5c9313c82b0edc7cf477163f28403413b4a6
jimtnnr <jimtnnr@gmail.com>
2025-05-12 12:30:06 -0500

    center screen

--- AIB6/MainWindow.axaml.cs
+++ AIB6/MainWindow.axaml.cs
@@ -7,6 +7,7 @@ namespace AIB6
         public MainWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
     }
 }
